6f0193405cd525dbd710748e91b4b559406f442d
Andrew Zitek <zitek936@gmail.com>
2020-12-29 11:26:15 -0500

    main: checkpoint

--- src/controllers/news/main.go
+++ src/controllers/news/main.go
@@ -14,11 +14,21 @@ import (
   "github.com/z-tech/blue/src/datalayers/map"
 )
 
+type ArticleIDSchema struct {
+  ArticleID string `json:"ArticleID"`
+}
+
 // TODO(z-tech): what are the fields we actually want?
 type PostNewsSchema struct {
   ArticleBody string `json:"ArticleBody"`
   Author string `json:"Author"`
   Dateline string `json:"Dateline"`
+}
+
+func (articleIDSchema ArticleIDSchema) Validate() error {
+  return validation.ValidateStruct(&articleIDSchema,
+    validation.Field(&articleIDSchema.ArticleID, validation.Required, validation.Length(44, 44)),
+  )
 }
 
 func (postNewsSchema PostNewsSchema) Validate() error {
@@ -40,6 +50,25 @@ func ValidatePostNews(ctx *gin.Context) {
   ctx.Set("postNewsSchema", postNewsSchema)
 
   validateErr := postNewsSchema.Validate()
+  if validateErr != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("one or more properties in request body are not valid: %s", validateErr)})
+    ctx.Abort()
+    return
+  }
+}
+
+func ValidatePostNewsRevision(ctx *gin.Context) {
+  ValidatePostNews(ctx)
+  articleIDSchema := ArticleIDSchema{}
+  bindErr := ctx.ShouldBindWith(&articleIDSchema, binding.JSON)
+  if bindErr != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"error": bindErr.Error()})
+    ctx.Abort()
+    return
+  }
+  ctx.Set("articleID", articleIDSchema.ArticleID)
+
+  validateErr := articleIDSchema.Validate()
   if validateErr != nil {
     ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("one or more properties in request body are not valid: %s", validateErr)})
     ctx.Abort()
@@ -81,10 +110,62 @@ func PostNews(ctx *gin.Context) {
   }
 
   if isDup == true {
-    ctx.JSON(200, gin.H{"proof": proof, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
+    ctx.JSON(200, gin.H{"ArticleID": key, "proof": proof, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
     ctx.Abort()
     return
   }
 
-  ctx.JSON(201, gin.H{"proof": proof, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
+  ctx.JSON(201, gin.H{"ArticleID": key, "proof": proof, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
+}
+
+func PostNewsRevision(ctx *gin.Context) {
+  articleIDRaw, _ := ctx.Get("articleID")
+  articleID, isString := articleIDRaw.(string)
+  if isString == false {
+    ctx.JSON(http.StatusInternalServerError, gin.H{})
+    ctx.Abort()
+    return
+  }
+
+  postNewsSchema, _ := ctx.Get("postNewsSchema")
+  leafData, marshalErr := json.Marshal(postNewsSchema)
+  if marshalErr != nil {
+    ctx.JSON(http.StatusInternalServerError, gin.H{})
+    ctx.Abort()
+    return
+  }
+
+  proof, isDup, getProofErr := logDatalayer.AddLeaf(ctx, leafData)
+  if getProofErr != nil {
+    ctx.JSON(http.StatusInternalServerError, gin.H{})
+    ctx.Abort()
+    return
+  }
+
+  fmt.Printf("HLLOO %+v %v %v\n", proof, isDup, articleID)
+
+  // hash := []byte(articleID)
+  // key := hash[:]
+  // if (isDup == false) {
+  //   addLeafErr := mapDatalayer.AddLeaf(ctx, key, leafData)
+  //   if addLeafErr != nil {
+  //     ctx.JSON(http.StatusInternalServerError, gin.H{})
+  //     ctx.Abort()
+  //     return
+  //   }
+  // }
+  // mapLeaf, mapRoot, getLeafErr := mapDatalayer.GetLeaf(ctx, key)
+  // if getLeafErr != nil {
+  //   ctx.JSON(http.StatusInternalServerError, gin.H{})
+  //   ctx.Abort()
+  //   return
+  // }
+  //
+  // if isDup == true {
+  //   ctx.JSON(200, gin.H{"proof": proof, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
+  //   ctx.Abort()
+  //   return
+  // }
+  //
+  // ctx.JSON(201, gin.H{"proof": proof, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
 }
--- src/routes/news/main.go
+++ src/routes/news/main.go
@@ -8,4 +8,5 @@ import (
 
 func ApplyToEngine(engine *gin.Engine) {
   engine.POST("/v1/news", newsController.ValidatePostNews, newsController.PostNews)
+  engine.POST("/v1/news/revision", newsController.ValidatePostNewsRevision, newsController.PostNewsRevision)
 }
