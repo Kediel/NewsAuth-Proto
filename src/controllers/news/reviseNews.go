d8d2ea255d8479f47f0c950b349810b088db3d28
Andrew Zitek <zitek936@gmail.com>
2021-01-02 22:29:32 -0500

    main: split controllers

--- src/controllers/news/reviseNews.go
+++ src/controllers/news/reviseNews.go
@@ -18,43 +18,10 @@ type ArticleIDSchema struct {
   ArticleID string `json:"ArticleID"`
 }
 
-// TODO(z-tech): what are the fields we actually want?
-type PostNewsSchema struct {
-  ArticleBody string `json:"ArticleBody"`
-  Author string `json:"Author"`
-  Dateline string `json:"Dateline"`
-}
-
 func (articleIDSchema ArticleIDSchema) Validate() error {
   return validation.ValidateStruct(&articleIDSchema,
     validation.Field(&articleIDSchema.ArticleID, validation.Required, validation.Length(44, 44)),
   )
-}
-
-func (postNewsSchema PostNewsSchema) Validate() error {
-  return validation.ValidateStruct(&postNewsSchema,
-    validation.Field(&postNewsSchema.ArticleBody, validation.Required, validation.Length(1, 20000)),
-    validation.Field(&postNewsSchema.Author, validation.Required, validation.Length(1, 1000)),
-    validation.Field(&postNewsSchema.Dateline, validation.Length(0, 1000)),
-  )
-}
-
-func ValidatePostNews(ctx *gin.Context) {
-  postNewsSchema := PostNewsSchema{}
-  bindErr := ctx.ShouldBindBodyWith(&postNewsSchema, binding.JSON)
-  if bindErr != nil {
-    ctx.JSON(http.StatusBadRequest, gin.H{"error": bindErr.Error()})
-    ctx.Abort()
-    return
-  }
-  ctx.Set("postNewsSchema", postNewsSchema)
-
-  validateErr := postNewsSchema.Validate()
-  if validateErr != nil {
-    ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("one or more properties in request body are not valid: %s", validateErr)})
-    ctx.Abort()
-    return
-  }
 }
 
 func ValidatePostNewsRevision(ctx *gin.Context) {
@@ -76,41 +43,6 @@ func ValidatePostNewsRevision(ctx *gin.Context) {
     return
   }
   ctx.Set("articleID", articleIDSchema.ArticleID)
-}
-
-func PostNews(ctx *gin.Context) {
-  postNewsSchema, _ := ctx.Get("postNewsSchema")
-  leafData, marshalErr := json.Marshal(postNewsSchema)
-  if marshalErr != nil {
-    ctx.JSON(http.StatusInternalServerError, gin.H{})
-    ctx.Abort()
-    return
-  }
-
-  leafIndex, treeSize, proof, rootHash, leafHash, isDup, addLeafErr := logDatalayer.AddLeaf(ctx, leafData)
-  if addLeafErr != nil {
-    ctx.JSON(http.StatusInternalServerError, gin.H{})
-    ctx.Abort()
-    return
-  }
-
-  key := leafHash[:]
-  if (isDup == false) {
-    addLeafErr := mapDatalayer.AddLeaf(ctx, key, leafData)
-    if addLeafErr != nil {
-      ctx.JSON(http.StatusInternalServerError, gin.H{})
-      ctx.Abort()
-      return
-    }
-  }
-  mapLeaf, mapRoot, getLeafErr := mapDatalayer.GetLeaf(ctx, key)
-  if getLeafErr != nil {
-    ctx.JSON(http.StatusInternalServerError, gin.H{})
-    ctx.Abort()
-    return
-  }
-
-  ctx.JSON(200, gin.H{"LogProof": proof, "LogLeafIndex": leafIndex, "LogTreeSize": treeSize, "LogRootHash": rootHash, "LogLeafHash": leafHash, "mapLeaf": mapLeaf, "mapRoot": mapRoot})
 }
 
 func PostNewsRevision(ctx *gin.Context) {
